6296a75f8ad93a7aca6e6708a23d6220a4b816e9
Ian Scott <chesnekov@gmail.com>
2020-07-23 11:23:09 -0700

    updates Game class logic with a constructor and a private deck list, with a getter.
    Passes test method ensuring deck is instantiated with correct number of cards
    
    Co-authored-by: Tyson Lackey <lackeyt90@gmail.com>

--- GoFish.Tests/Models/Game.Tests.cs
+++ GoFish.Tests/Models/Game.Tests.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using GoFish.Models;
+using System.Collections.Generic;
+using System;
 
 namespace GoFish.Tests
 {
@@ -13,11 +15,12 @@ namespace GoFish.Tests
       Assert.AreEqual(typeof(Game), testGame.GetType());
     }
 
+    [TestMethod]
     public void GameConstructor_ContainsDeckOf52Cards_CardList()
     {
       Game testGame = new Game();
       testGame.PopulateDeck();
-      Assert.AreEqual(52, testGame.Deck.Count);
+      Assert.AreEqual(52, testGame.GetDeck().Count);
     }
   }
 }
--- GoFish/Models/Game.cs
+++ GoFish/Models/Game.cs
@@ -1,6 +1,37 @@
+using System.Collections.Generic;
+
 namespace GoFish.Models
 {
   public class Game
   {
-    
+    public int TurnCount { get; set; }
+    private List<string[]> _deck = new List<string[]> {};
+
+    public Game()
+    {
+      TurnCount = 1;
+    }
+
+    public void PopulateDeck()
+    {
+      string[] values = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+      string[] suites = {"Spades", "Clubs", "Diamonds", "Hearts"};
+      string[] temp = {"", ""};
+
+      foreach (string value in values)
+      {
+        foreach (string suite in suites)
+        {
+          temp[0] = value;
+          temp[1] = suite;
+          _deck.Add(temp);
+        }
+      }
+    }
+
+    public List<string[]> GetDeck()
+    {
+      return _deck;
+    }
   }
+}
